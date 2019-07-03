0e23f66b083c82cf21daf1d0fc160d68f1a35c75
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-07-03 16:15:51 -0500

    loop it

--- codewars/loopdy-loops.py
+++ codewars/loopdy-loops.py
@@ -7,24 +7,26 @@ names = 'riley', 'mhyssa', 'mom', 'scoop'
 nums = 1, 2, 3, 4, 5, 6
 
 
-def make_list(*args):
+def make_list(strings):
     _list = []
-    for arg in args:
-        _list.append(arg)
+    for string in strings:
+        _list.append(string)
     return _list
 
 
-def list_details(*args):
-    print('This is the list: %s' % args)
-    # try:
-    #     print(f'''
-    #           The list, {args}, has a length of {len(args)}. \n\n
-    #           This is {args} flattened:
-    #           {', '.join(args)}
-    #           ''')
-    # except Exception:
-    #     print(f'{type(args)}')
-    #     raise
+def list_details(_list, *_lists):
+    # print('This is the list: %s\n' % list)
+    try:
+        print(
+            f"List: {_list} \n"
+            f'Length: {len(_list)}. \n'
+            f"Flattened: {', '.join(str(arg) for arg in _list)} \n"
+            f"Type: {type(_list)} \n"
+        )
+    except Exception:
+        print("There was an error.")
+        print(f'{type(_list)}')
+        raise
 
 
 def compare_objects():
@@ -37,9 +39,17 @@ def compare_objects():
 
 
 ml = make_list(names)
-print(ml, *ml, sep='\n')
-print(f'''
-      ml  : {type(ml)}
-      *ml : {type((*ml))}
-      ()  : {type(('just', 'some', 'list'))}
-      ''')
+
+# ld = list_details(ml)
+
+list_details(ml)
+
+# list_details(ml)
+# list_details(*ml)
+
+# print(ml, *ml, sep='\n')
+# print(f'''
+#       ml  : {type(ml)}
+#       *ml : {type((*ml,))}
+#       ()  : {type(('just', 'some', 'list'))}
+#       ''')
