b2107c88d27b8ec70016f9826318d364d4ba8e88
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-07-06 17:27:53 -0500

    methods

---
+++ method-practice/met-prac.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+def add_two_to(self, a, b):
+    x = self + a
+    y = self + b
+    return self, x, y
+
+
+add_two_to(1, 4, 6)
+
+myObj = 3
+
+mySecObj = add_two_to(myObj, 3, 3)
+
+# print(f"obj: {myObj}, type: {type(myObj)}")
+# print(f"obj: {mySecObj}, type: {type(mySecObj)}")
+
+
+def obj_info(*object):
+    for i in object:
+        print(f"{i}: {type(i)}")
+
+
+obj_info(myObj)
