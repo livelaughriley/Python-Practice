56de6b59c1a4e8e690e0fa420c4ddf598e559059
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-07-08 19:40:35 -0500

    from lists to dictionaries

---
+++ parameters/make-params.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def group_overlays(groups: list, overlays: list):
+    x = {}
+    for group in groups:
+        for overlay in overlays:
+            x[group] = overlay
+            overlays.remove(overlay)
+    return x
+
+
+a = 'Education'
+b = ['Class Year', 'Degree']
+c = "Donor"
+d = ["Donor Status", "Lifetime Giving"]
+
+z = ['Education', 'Donor']
+zz = [['Class Year', 'Degree'], ['Donor Status', 'Lifetime Giving']]
+
+e = group_overlays([a, c], [b, d])
+parameters = group_overlays(z, zz)
+
+for group in parameters:
+    for overlay in parameters[group]:
+        print(f"{group}: {overlay}")
