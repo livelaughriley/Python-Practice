26c7c675c59f2ed6be2b221026cf4ecb09e74977
livelaughriley <34988536+livelaughriley@users.noreply.github.com>
2019-08-16 11:25:27 -0500

    remove unnecessary comments

--- looops/nested-loops.py
+++ looops/nested-loops.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python3
 A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-"""
-    print("A: ", type(A))
-    print(B, type(B))
-    print("A[1][1]: ", type(A[1][1]), A[1][1])
-    print("A[1]: ", type(A[1]), A[1])
-    for i in range(0, 3):
-        print(type(i))
-"""
-
 
 def list_loop(obj: list, x: int, verbose: bool = False):
     for i in range(0, 3):
