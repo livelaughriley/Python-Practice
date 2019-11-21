9920376d977908eee30acae554a8a10c3d0f3ed4
rileytwo <34988536+rileytwo@users.noreply.github.com>
2019-11-21 11:38:34 -0600

    add some truth stuff

---
+++ misc/truth-logic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+x: bool = True
+y: bool = True
+z: bool = False
+a: bool = False
+
+if x is not True and y is not True:
+    print("true")
+else:
+    print("false")
+
+
+if z is not True and a is not True:
+    print("true")
+else:
+    print("false")
